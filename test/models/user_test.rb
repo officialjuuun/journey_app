51ec9c9e24827502c89693ef3bbe2bd6e4f9ef07
offcialjuuun <jun2fute@example.com>
2022-06-18 03:58:33 +0000

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -67,4 +67,10 @@ class UserTest < ActiveSupport::TestCase
   end
   
   
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+    
+  
+  
 end
