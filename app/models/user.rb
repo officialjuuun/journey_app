db7afe6d9552cb4a25f19c73f1b2dc1ea17bc390
offcialjuuun <jun2fute@example.com>
2022-07-12 10:57:30 +0000

    ttt

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
                                    dependent:   :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  attr_accessor :remember_token, :reset_token
+  attr_accessor :remember_token
   before_save   :downcase_email
   validates :name,  presence: true, length: { maximum: 50 }
   
@@ -48,23 +48,6 @@ class User < ApplicationRecord
     self.update_attribute(:remember_digest, nil)
   end
   
-  # パスワード再設定の属性を設定する
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
-
-  # パスワード再設定のメールを送信する
-  def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
-  end
-  
-  # パスワード再設定の期限が切れている場合はtrueを返す
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
-  
   # 試作feedの定義
   # 完全な実装は次章の「ユーザーをフォローする」を参照
           #↓
@@ -99,12 +82,6 @@ class User < ApplicationRecord
     # メールアドレスをすべて小文字にする
     def downcase_email
       self.email = email.downcase
-    end
-
-    # 有効化トークンとダイジェストを作成および代入する
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
     end
   
 end
