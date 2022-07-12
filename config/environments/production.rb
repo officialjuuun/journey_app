084a9435ffad5ba7cad2ef7065752e3fa3abe6a8
offcialjuuun <jun2fute@example.com>
2022-07-12 12:02:22 +0000

    account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,18 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # con
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'warm-cove-71637.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
+    :authentication => :plain,
+  }
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   
